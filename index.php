83bc75e2ababb446893da185a494ce484fc7fa9e
Luca Giampieri <luca.giampieri95@gmail.com>
2026-04-13 16:04:27 +0200

    Aggiunta lista dischi a schermo e sistemazione grafica generale

--- index.php
+++ index.php
@@ -7,20 +7,32 @@ require_once './functions.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./style.css">
     <title>php-dischi-json</title>
 </head>
 <body>
-    <h1>Lista Dischi</h1>
+    <header>
+        <img src="./img/vinile.png" alt="vinile">
+        <h1 class="header-title">Our selection of records:</h1>
+    </header>
 
-    <ul>
-        <?php
-    foreach ($discs as $disc) {
-        ?> <hr> <?php
-        foreach ($disc as $date) {
-            echo "<li>$date </li>";
-        };
-    };
+    <main>
+        <div class="container">
+  <?php
+foreach ($discs as $disc) {
+    ?>
+    <div class="card">
+        <img src="<?= $disc['cover'] ?>" alt="cover" >
+        <h2><?= $disc['artista'] ?></h2>
+        <p><?= $disc['titolo'] ?></p>
+        <p><?= $disc['anno'] ?></p>
+        <p><?= $disc['genere'] ?></p>
+    </div>
+<?php
+}
 ?>
-    </ul>
+        </div>
+    </main>
 </body>
 </html>
